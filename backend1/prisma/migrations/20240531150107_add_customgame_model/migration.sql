552506238b4a98bb835f7918bf83cc8432f230e7
Sanyog Dani <sanyogdani1967@gmail.com>
2025-02-07 21:25:43 +0530

    added

--- backend1/prisma/migrations/20240531150107_add_customgame_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "CustomGame" (
-    "id" TEXT NOT NULL,
-    "player" TEXT NOT NULL,
-
-    CONSTRAINT "CustomGame_pkey" PRIMARY KEY ("id")
-);
--- backend1/src/messages.ts
+++
@@ -1,7 +0,0 @@
-export const INIT_GAME = "init_game";
-export const MOVE = "move";
-export const GAME_OVER = "game_over";
-export const CUSTOM_GAME = "custom_game";
-export const REDIRECT = "redirect";
-export const START_CUSTOM = "start_custom";
-export const MESSAGE_RECEIVED = "message_received";
